414275ec6cdd1dfe6378772448dfb079a45ade31
Steve jobs <mobitrapp@gmail.com>
2016-01-16 11:41:17 +0530

    Added crashytics to the App.

---
+++ Bulletin/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Bulletin
+//
+//  Created by Shailesh Chandavar on 15/01/16.
+//  Copyright © 2016 com.Mobitrapp. All rights reserved.
+//
+
+import UIKit
+import Crashlytics
+import Fabric
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func crashButtonTapped(sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
+
+    }
+
+}
+
