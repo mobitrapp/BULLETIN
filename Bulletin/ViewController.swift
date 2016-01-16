c43f23b15dfb572d17e4fe4bb1f8edfd3ba2b5b9
Steve jobs <mobitrapp@gmail.com>
2016-01-16 12:18:08 +0530

    Added code to enable debug mode for the app.

--- Bulletin/ViewController.swift
+++ Bulletin/ViewController.swift
@@ -11,21 +11,16 @@ import Crashlytics
 import Fabric
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func crashButtonTapped(sender: AnyObject) {
-        Crashlytics.sharedInstance().crash()
-
-    }
-
+    
 }
 
